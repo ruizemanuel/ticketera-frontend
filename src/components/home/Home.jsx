f24161cf0528b0453e5d008a4c3ff89e4aebc159
Emanuel Ruiz <ruiz.e.642@gmail.com>
2024-02-24 17:05:46 -0300

    modified some styles

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -11,28 +11,19 @@ const Home = () => {
         fontSize: "1.5rem"
     };
 
-    const cardsLayout = {
-        display: "grid",
-        gridTemplateColumns: "repeat(auto-fit, minmax(250px, 1fr))",
-        gridRowGap: "80px",
-        gridColumnGap: "40px",
-        maxWidth: "900px",
-        margin: "0 auto 30px"
-    };
-
     return (
         <div>
             <Container className="py-5">
                 {tickets.length === 0 ? (
                     <p style={messageStyle}>No hay tickets disponibles.</p>
                 ) : (
-                    <Row style={cardsLayout}>
+                    <div className="cards-layout">
                         {tickets.map((item, index) => (
                             <Col key={index} xs={12} sm={6} md={4} lg={3}>
                                 <Card data={item} />
                             </Col>
                         ))}
-                    </Row>
+                    </div>
                 )}
             </Container>
         </div>
